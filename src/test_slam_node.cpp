de636e2604cee6878448f05bdc2415669a13a3a5
fengyu <992491260@qq.com>
2018-12-07 17:43:41 +0800

    add map

--- src/test_slam_node.cpp
+++ src/test_slam_node.cpp
@@ -1,14 +1,17 @@
 #include "test_slam_node.hpp"
 #include "common.hpp"
 
-#include <nav_msgs/OccupancyGrid.h>
-#include <nav_msgs/Odometry.h>
+
 
 #include "boost/asio.hpp"   //包含boost库函数
 #include "boost/bind.hpp"
 #include <iostream>
+#include <string>
 #include <math.h>
 
+using namespace slam;
+
+const std::string kMapTopic_ = "map";
 
 //构造函数
 TestSlam::TestSlam(tf::TransformListener *tf)
@@ -16,7 +19,19 @@ TestSlam::TestSlam(tf::TransformListener *tf)
     ros::NodeHandle private_nh_("~");
     tf_ = tf;
 
+    tfB_ = new tf::TransformBroadcaster();
+
     param_.GetParam(&nh_);
+
+    
+    sensor_data_manager_ = std::make_shared<slam::SensorDataManager>();
+    slam_processer_ = std::make_unique<slam::SlamProcessor>(sensor_data_manager_, 0.05);
+
+    
+    map_publisher_ = nh_.advertise<nav_msgs::OccupancyGrid>(kMapTopic_, 1, true);
+    mpa_pub_thread_ = new std::thread(&TestSlam::MapPublishLoop, this, 1);
+    first_map_ = true;
+    
 
     scan_pos_cal.setZero();
     odom_pos_cal.setZero();
@@ -34,6 +49,10 @@ TestSlam::TestSlam(tf::TransformListener *tf)
     scan_path_pub_ = nh_.advertise<nav_msgs::Path>("scan_path_pub_",1,true);
     path_scan.header.stamp=current_time_;
     path_scan.header.frame_id=param_.global_frame_id;
+
+    odom_path_pub_ = nh_.advertise<nav_msgs::Path>("odom_path_pub_",1,true);
+    path_odom.header.stamp=current_time_;
+    path_odom.header.frame_id=param_.global_frame_id;
 
     optimization_path_pub_ = nh_.advertise<nav_msgs::Path>("optimization_path_pub_",1,true);
     path_optimization_.header.stamp=current_time_;
@@ -131,7 +150,7 @@ void TestSlam::scanCallBack(const sensor_msgs::LaserScan::ConstPtr &laserScanMsg
 
     sensor_msgs::LaserScan scan = *laserScanMsg;
 
-    laser_data_process_->ScanDataCalibrate(scan);
+    // laser_data_process_->ScanDataCalibrate(scan);
 
     if(!getTfPose(param_.odom_frame_id, param_.base_frame_id, odom_base_pose, scan.header.stamp)){
         d_odom = Eigen::Vector3d::Zero();
@@ -154,65 +173,256 @@ void TestSlam::scanCallBack(const sensor_msgs::LaserScan::ConstPtr &laserScanMsg
         odom_pos_cal(1) += s*d_odom(0) + c*d_odom(1);
         odom_pos_cal(2) += d_odom(2);
     }
+    pubPathMsg(odom_pos_cal, path_odom, odom_path_pub_);
 
 
 
 //    d_odom = Eigen::Vector3d::Zero();
-    d_scan_laser = scan_matcher_.ResolveScanMatch(&scan, d_odom);
-    getTfPose(param_.base_frame_id, param_.laser_frame_id,
-              base_laser_pose, scan.header.stamp);
-    d_scan_base = PoseToTrans(base_laser_pose) * d_scan_laser;
-    c = cos(scan_pos_cal(2));
-    s = sin(scan_pos_cal(2));
-    scan_pos_cal(0) += c*d_scan_base(0) - s*d_scan_base(1);
-    scan_pos_cal(1) += s*d_scan_base(0) + c*d_scan_base(1);
-    scan_pos_cal(2) += d_scan_base(2);
+    // d_scan_laser = scan_matcher_.ResolveScanMatch(&scan, d_odom);
+    // getTfPose(param_.base_frame_id, param_.laser_frame_id,
+    //           base_laser_pose, scan.header.stamp);
+    // d_scan_base = PoseToTrans(base_laser_pose) * d_scan_laser;
+    // c = cos(scan_pos_cal(2));
+    // s = sin(scan_pos_cal(2));
+    // scan_pos_cal(0) += c*d_scan_base(0) - s*d_scan_base(1);
+    // scan_pos_cal(1) += s*d_scan_base(0) + c*d_scan_base(1);
+    // scan_pos_cal(2) += d_scan_base(2);
+    // pubPathMsg(scan_pos_cal, path_scan, scan_path_pub_);
 
-    pubPathMsg(scan_pos_cal, path_scan, scan_path_pub_);
+{
+    // pose_graph_.AddVertex({scan_pos_cal}, vertex_counter_++);
+    // if(vertex_counter_ > 1){
+    //     Edge tmp_e = {vertex_counter_-2, vertex_counter_-1, d_odom, info_matrix};
+    //     pose_graph_.AddEdge(tmp_e);
+    // }
+    // if(vertex_counter_ == 1000) {
+    //     pose_graph_.PoseGraphOptimization(0, pose_graph_.GetVertex().size());
+    // }else if(!(vertex_counter_%10) && vertex_counter_ >= 30){
+    //     pose_graph_.PoseGraphOptimization(vertex_counter_-20, 20);
+    // }
+}
+
+{
+        // path_optimization_.poses.clear();
+        // for(auto & vertex : pose_graph_.GetVertex())
+        // {
+        //     Eigen::Vector3d pose = vertex.second.pose;
+
+        //     current_time_ = ros::Time::now();
+        //     geometry_msgs::PoseStamped v_pose_stamped;
+        //     v_pose_stamped.pose.position.x = pose(0);
+        //     v_pose_stamped.pose.position.y = pose(1);
+        //     geometry_msgs::Quaternion q = tf::createQuaternionMsgFromYaw(pose(2));
+        //     v_pose_stamped.pose.orientation.x = q.x;
+        //     v_pose_stamped.pose.orientation.y = q.y;
+        //     v_pose_stamped.pose.orientation.z = q.z;
+        //     v_pose_stamped.pose.orientation.w = q.w;
+        //     v_pose_stamped.header.stamp=current_time_;
+        //     v_pose_stamped.header.frame_id=param_.global_frame_id;
+
+        //     path_optimization_.poses.push_back(v_pose_stamped);
+        // }
+        // pubPathMsg(pose_graph_.GetVertex()[vertex_counter_-2].pose,path_optimization_,optimization_path_pub_);
+
+}
 
 
-    pose_graph_.AddVertex({scan_pos_cal}, vertex_counter_++);
-    if(vertex_counter_ > 1){
-        Edge tmp_e = {vertex_counter_-2, vertex_counter_-1, d_odom, info_matrix};
-        pose_graph_.AddEdge(tmp_e);
+ ros::WallTime startTime = ros::WallTime::now();
+    if(sensor_data_manager_->GetRangeFinder() == nullptr){
+        std::unique_ptr<slam::LaserRangeFinder> range_finder = std::make_unique<slam::LaserRangeFinder>(scan.angle_min, scan.angle_max, scan.angle_increment,
+                                                              scan.range_min, scan.range_max);
+        sensor_data_manager_->SetRangeFinder(std::move(range_finder));                                         
     }
 
+    size_t size = scan.ranges.size();
+    std::shared_ptr<slam::RangeDataContainer2d> range_data_container = std::make_shared<slam::RangeDataContainer2d>(size);
+    range_data_container->set_sensor_origin(Eigen::Vector2d::Zero());
+    double current_index_angle = scan.angle_min;
+    double range_threshold = sensor_data_manager_->GetRangeFinder()->get_range_threshold();
+    double resolution = slam_processer_->get_map_resolution();
+    for(size_t i = 0; i < size; ++i){
+        double dist = scan.ranges[i];
 
-    if(vertex_counter_ == 768) {
-        pose_graph_.PoseGraphOptimization(0, pose_graph_.GetVertex().size());
-    }else if(!(vertex_counter_%10) && vertex_counter_ >= 30){
-        pose_graph_.PoseGraphOptimization(vertex_counter_-20, 20);
-    }
-
-    {
-        path_optimization_.poses.clear();
-        for(auto & vertex : pose_graph_.GetVertex())
+        if((dist > scan.range_min) && (dist < range_threshold))
         {
-            Eigen::Vector3d pose = vertex.second.pose;
-
-            current_time_ = ros::Time::now();
-            geometry_msgs::PoseStamped v_pose_stamped;
-            v_pose_stamped.pose.position.x = pose(0);
-            v_pose_stamped.pose.position.y = pose(1);
-            geometry_msgs::Quaternion q = tf::createQuaternionMsgFromYaw(pose(2));
-            v_pose_stamped.pose.orientation.x = q.x;
-            v_pose_stamped.pose.orientation.y = q.y;
-            v_pose_stamped.pose.orientation.z = q.z;
-            v_pose_stamped.pose.orientation.w = q.w;
-            v_pose_stamped.header.stamp=current_time_;
-            v_pose_stamped.header.frame_id=param_.global_frame_id;
-
-            path_optimization_.poses.push_back(v_pose_stamped);
+            dist /= resolution;
+            range_data_container->AddDataPoint(Eigen::Vector2d(cos(current_index_angle) * dist, sin(current_index_angle) * dist));
         }
-        pubPathMsg(pose_graph_.GetVertex()[vertex_counter_-2].pose,path_optimization_,optimization_path_pub_);
 
+        current_index_angle += scan.angle_increment;
     }
 
+    Eigen::Vector3d predict_sensor_pose = best_pose_;
+    predict_sensor_pose(0) += cos(odom_pos_cal(2))*d_odom(0) - sin(odom_pos_cal(2))*d_odom(1);
+    predict_sensor_pose(1) += sin(odom_pos_cal(2))*d_odom(0) + cos(odom_pos_cal(2))*d_odom(1);
+    predict_sensor_pose(2) += d_odom(2);
+    range_data_container->set_sensor_pose(predict_sensor_pose);
 
-    std::cout << "Data Cnt:" << dataCnt++ << std::endl;
+    sensor_data_manager_->AddSensorData(range_data_container, slam::OdometryData(odom_base_pose));
+
+
+    slam_processer_->process();
+
+ros::WallDuration duration = ros::WallTime::now() - startTime;
+ROS_INFO("TestSLAM took: %f milliseconds", duration.toSec()*1000.0f );
+
+    best_pose_ = slam_processer_->GetCurrentSensorPose();
+    publishTf(scan.header.stamp);
+
+    // std::cout << "Data Cnt:" << dataCnt++ << std::endl;
+}
+
+const bool kDisplayFullMap = true;
+void TestSlam::MapPublishLoop(float map_pub_period){
+
+    ros::Rate r(1.0 / map_pub_period);
+    while(ros::ok())
+    {
+
+        ros::WallTime t1 = ros::WallTime::now();
+
+        // std::lock_guard<std::mutex> lock(map_mutex_);
+        slam_processer_->GetMapMutex().lock();
+
+        if(first_map_){
+            map_.map.info.resolution = slam_processer_->get_map_resolution();
+            map_.map.info.origin.position.x = 0.0;
+            map_.map.info.origin.position.y = 0.0;
+            map_.map.info.origin.position.z = 0.0;
+            map_.map.info.origin.orientation.x = 0.0;
+            map_.map.info.origin.orientation.y = 0.0;
+            map_.map.info.origin.orientation.z = 0.0;
+            map_.map.info.origin.orientation.w = 1.0;
+
+            first_map_ = false;
+        }
+
+        std::shared_ptr<slam::Map> occu_grid_map = slam_processer_->GetPubMap();
+        if(occu_grid_map != nullptr){
+
+            double resolution = occu_grid_map->GetCellLength();
+
+            if(kDisplayFullMap){
+                Eigen::Vector2d map_origin(occu_grid_map->GetWorldCoords(Eigen::Vector2d::Zero()));
+                map_origin.array() -= occu_grid_map->GetCellLength()*0.5;
+                map_.map.info.origin.position.x = map_origin.x();
+                map_.map.info.origin.position.y = map_origin.y();
+                map_.map.info.origin.orientation.w = 1.0;
+
+                map_.map.info.resolution = resolution;
+
+                map_.map.info.width = occu_grid_map->GetSizeX();
+                map_.map.info.height = occu_grid_map->GetSizeY();
+
+                int size = map_.map.info.width * map_.map.info.height;
+                map_.map.data.resize(size);
+                std::vector<int8_t>& data = map_.map.data;
+                memset(&data[0], -1, sizeof(int8_t) * size);
+                // int size = occu_grid_map->GetGridCellNum();
+                for(int i = 0; i < size; ++i){
+                    switch (occu_grid_map->GetGridStates(i)){
+                        case GridStates_Occupied:{
+                            data[i] = 100;
+                            break;
+                        }
+                        case GridStates_Free:{
+                            data[i] = 0;
+                            break;
+                        }
+                        case GridStates_Unknown:{
+                            data[i] = -1;
+                            break;
+                        }
+                        default:
+                            break;
+                    }
+                }
+            }else{
+                Eigen::Vector2d map_origin(occu_grid_map->GetWorldCoords(occu_grid_map->GetStartGrid()));
+                map_origin.array() -= occu_grid_map->GetCellLength()*0.5;
+                map_.map.info.origin.position.x = map_origin.x();
+                map_.map.info.origin.position.y = map_origin.y();
+                map_.map.info.origin.orientation.w = 1.0;
+                
+                map_.map.info.resolution = resolution;
+
+                map_.map.info.width = (occu_grid_map->GetBoundSizeX());
+                map_.map.info.height = (occu_grid_map->GetBoundSizeY());
+
+                int size = map_.map.info.width * map_.map.info.height;
+                map_.map.data.resize(size, static_cast<int8_t>(-1));
+                std::vector<int8_t>& data = map_.map.data;
+
+                int data_index = 0;
+                int start_x = occu_grid_map->GetStartGrid()[0];
+                int start_y = occu_grid_map->GetStartGrid()[1];
+                int end_x = occu_grid_map->GetEndGrid()[0];
+                int end_y = occu_grid_map->GetEndGrid()[1];
+
+                for(int j = start_y; j <= end_y; ++j){
+                    for(int i = start_x; i <= end_x; ++i){
+                    
+                        switch (occu_grid_map->GetGridStates(i,j)){
+                            case GridStates_Occupied:{
+                                data.at(data_index) = 100;
+                                break;
+                            }
+                            case GridStates_Free:{
+                                data.at(data_index) = 0;
+                                break;
+                            }
+                            case GridStates_Unknown:{
+                                data.at(data_index) = -1;
+                                break;
+                            }
+                            default:
+                                break;
+                        }
+                        data_index++;
+
+                    }
+                }
+            }
+
+        }
+        
+        
+        map_.map.header.stamp = ros::Time::now();
+        map_.map.header.frame_id = param_.global_frame_id;
+       
+        map_publisher_.publish(map_.map);
+
+
+        ros::WallDuration t2 = ros::WallTime::now() - t1;
+        std::cout << "map pub time s: " << t2.toSec() << std::endl;
+
+        slam_processer_->GetMapMutex().unlock();
+
+        r.sleep();
+    }
 }
 
 
 
+void TestSlam::publishTf(ros::Time time_stamp)
+{
+    tf::StampedTransform odom_to_laser;
+
+    try
+    {
+      tf_->waitForTransform(param_.odom_frame_id, param_.laser_frame_id, time_stamp, ros::Duration(0.5));
+      tf_->lookupTransform(param_.odom_frame_id, param_.laser_frame_id, time_stamp, odom_to_laser);
+    }
+    catch(tf::TransformException e)
+    {
+      ROS_ERROR("Transform failed during publishing of map_odom transform: %s",e.what());
+      odom_to_laser.setIdentity();
+    }
+
+    tf::Transform map_to_odom =  tf::Transform(tf::Transform(tf::createQuaternionFromRPY(0, 0, best_pose_[2]),
+                                                            tf::Vector3(best_pose_[0], best_pose_[1], 0.0))
+                                                   * odom_to_laser.inverse());
+    tfB_->sendTransform( tf::StampedTransform (map_to_odom, time_stamp, param_.global_frame_id, param_.odom_frame_id));
+}
 
 
--- src/test_slam_node.hpp
+++ src/test_slam_node.hpp
@@ -2,12 +2,18 @@
 #define TEST_SLAM_NODE_H
 
 #include <iostream>
+#include <thread>
+#include <mutex>
 #include <eigen3/Eigen/Core>
 
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 #include <nav_msgs/Path.h>
+#include <nav_msgs/GetMap.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <nav_msgs/Odometry.h>
 #include <tf/transform_listener.h>
+#include <tf/transform_broadcaster.h>
 #include <tf/message_filter.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/synchronizer.h>
@@ -19,6 +25,9 @@
 #include "pose_graph_g2o.hpp"
 #include "pose_graph_ceres.hpp"
 #include "laser_data_process.hpp"
+
+#include "slam/sensor_data_manager.h"
+#include "slam/slam_processor.h"
 
 class TestSlam
 {
@@ -40,8 +49,10 @@ public:
     void pubPathMsg( Eigen::Vector3d& pose,nav_msgs::Path &path,ros::Publisher &path_pub_);
 
 
-
+    void publishTf(ros::Time time_stamp);
     Eigen::Vector3d  calcuDeltaOdom(Eigen::Vector3d odom_pose);
+
+    void MapPublishLoop(float map_pub_period);
 
 public:
     Eigen::Vector3d scan_pos_cal;
@@ -49,9 +60,13 @@ public:
 
     ros::NodeHandle nh_;
     tf::TransformListener *tf_;
+    tf::TransformBroadcaster* tfB_;
 
     ros::Publisher scan_path_pub_;
     nav_msgs::Path path_scan;
+
+    ros::Publisher odom_path_pub_;
+    nav_msgs::Path path_odom;
 
     ros::Publisher optimization_path_pub_;
     nav_msgs::Path path_optimization_;
@@ -61,6 +76,15 @@ public:
     Eigen::Vector3d now_pos_,last_pos_;
     std::vector<Eigen::Vector3d> odom_increments_;
 
+    std::thread* mpa_pub_thread_;
+    std::mutex map_mutex_;
+    ros::Publisher map_publisher_;
+    ros::Publisher map_metadata_publisher_;
+    nav_msgs::GetMap::Response map_;
+    bool first_map_;
+
+
+    Eigen::Vector3d best_pose_ = Eigen::Vector3d::Zero();
 
 private:
     ParamConfig param_;
@@ -73,6 +97,10 @@ private:
 
     int vertex_counter_;
     Eigen::Matrix3d info_matrix;
+
+
+    std::unique_ptr<slam::SlamProcessor> slam_processer_;
+    std::shared_ptr<slam::SensorDataManager> sensor_data_manager_;
 
 };
 
